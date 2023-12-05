33fcee9950f36135274183103411659af551e1e9
Lysender <developer@lysender.com>
2023-12-05 01:58:56 +0000

    fix: prepare for day5

---
+++ src/day5.rs
@@ -0,0 +1,23 @@
+
+pub fn part1(input: &str) -> u32 {
+   todo!() 
+}
+
+pub fn part2(input: &str) -> u32 {
+   todo!() 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+
+    }
+
+    #[test]
+    fn test_part2() {
+
+    }
+}
